9054df12dab9f1e09706dd743c27f505e04f33a9
Pakkva <65540336+Pakkva@users.noreply.github.com>
2021-03-14 13:19:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-import random
+import random, os
+p = 0
+print(os.walk("BD"))
 for i in range(20):
-    f = open(f"BD/{i}.txt", "w")
-    f.write(f"{random.randint (0, 1000)}")
-    f.close()
+    f = open(f"BD/{random.randint(850,950)}","w")
+    f.write(f"{random.randint(1,1000)}")
+    f.close
+
+for i in os.walk("BD"):
+    for j in i[2]:
+        print(j)
+        f = open(f"BD/{j}", "r")
+        a = f.read
+        print(a)
+        p += int(a)
+        f.close
+for i in range(20):
+    os.remove(j)
+print(p)
+
+
 
 
